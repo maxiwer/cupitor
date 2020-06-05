93f9edeb0843aa606ae9bf47eeb97a58aa450229
Max <fma1cr@gmail.com>
2020-06-05 12:04:05 +0600

    reading file and separating its text by enter is added

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const h1 = document.querySelector('.tesst');
+const uniqueWordsArr = document.querySelectorAll("th");
 
-h1.addEventListener('click', e => {
-    console.log('HEY!');
-});
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    let fr = new FileReader();
+    fr.onload = (x) => resolve(fr.result);
+    fr.readAsText(file);
+  });
+}
+
+async function read(input) {
+  const txtFile = await readFile(input.files[0]);
+  let arrBySpaceSplit = [];
+  let finalArr = [];
+
+  console.log(
+    "%c" + txtFile,
+    `background-color: darkBlue; color: white; font-size: 14px;`
+  );
+
+  let arrByEnterSplit = txtFile.split("\n");
+
+  if (arrByEnterSplit.length > 1) {
+    arrBySpaceSplit = arrByEnterSplit.map((element) => {
+      if (element.length > 0) {
+        return element.split(" ");
+      }
+    });
+    console.log("hey", arrBySpaceSplit);
+  } else {
+    arrBySpaceSplit = txtFile.trim().split(" ");
+  }
+}
+
+function onSubmit() {
+  console.log("heu");
+}
